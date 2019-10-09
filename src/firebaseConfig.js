64444c8ca1dd2b828fea379bed1d3796e65cda1d
Krina Soni <krina.soni@openxcellinc.com>
2019-10-09 17:14:20 +0530

    Initial Commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "test",
+    authDomain: "***",
+    databaseURL: "***",
+    projectId: "test",
+    storageBucket: "***",
+    messagingSenderId: "***",
+};
+
+export default config;
